cab9f720a496812b1e609bea3bac3773c10c0600
NeleFyrsten <152161825+NeleFyrsten@users.noreply.github.com>
2023-12-28 22:20:37 +0100

    animationer registrering, home side
    jeg har færdiggjort registreringssiden ift. at pageContent skifter, når man trykker på knappen. Det nåede jeg med JavaScript ved at tilføje og fjerne klasser fra siderne. Jeg har også begyndt med home-skærmen. Jeg måtte ændre nogle ting i css, så at det fungerer med alle 3 sider jeg indtil videre har programmeret. Det og at lære lidt om JavaScript tog lidt længere tid.

---
+++ Nele/NærboksApp/main.js
@@ -0,0 +1,19 @@
+function changePageContent(currentPageNumber) { // 0
+    var pageContentProgress = document.getElementsByClassName("pageContent--progress")
+    pageContentProgress[currentPageNumber].classList.add("pageContent--inactive")
+    pageContentProgress[currentPageNumber + 1].classList.remove("pageContent--inactive")
+
+    var progressBar__dot = document.getElementsByClassName("progressBar__dot")
+    progressBar__dot[currentPageNumber + 1].classList.add("progressBar__dot--active")
+
+    var progressBar__image = document.getElementsByClassName("progressBar__image")
+
+    if(currentPageNumber == 0) {
+        progressBar__image[0].classList.add("progressBar__image--position2")
+    }
+
+    if(currentPageNumber == 1) {
+        progressBar__image[0].classList.replace("progressBar__image--position2","progressBar__image--position3")
+    }
+}
+
