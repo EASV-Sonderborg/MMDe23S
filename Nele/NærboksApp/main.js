1d447c435ff29ecae1556ec536ec263a707a52a5
NeleFyrsten <152161825+NeleFyrsten@users.noreply.github.com>
2024-01-02 23:42:06 +0100

    Retur færdig
    Returprocessen er færdig. Der er et udråbstegn, der skal rettes, men ellers mangler der ikke noget.
    Jeg har arbejdet rigtig meget med active- og inactive-klasser her, fordi jeg har flere sider i hver skridt og mange små animationer (fx select-felte).

--- Nele/NærboksApp/main.js
+++ Nele/NærboksApp/main.js
@@ -1,7 +1,7 @@
 function changePageContent(currentPageNumber) { // 0
-    var pageContentProgress = document.getElementsByClassName("pageContent--progress")
-    pageContentProgress[currentPageNumber].classList.add("pageContent--inactive")
-    pageContentProgress[currentPageNumber + 1].classList.remove("pageContent--inactive")
+    var pageContent = document.getElementsByClassName("pageContent")
+    pageContent[currentPageNumber].classList.add("pageContent--inactive")
+    pageContent[currentPageNumber + 1].classList.remove("pageContent--inactive")
 
     var progressBar__dot = document.getElementsByClassName("progressBar__dot")
     progressBar__dot[currentPageNumber + 1].classList.add("progressBar__dot--active")
@@ -23,4 +23,111 @@ function menuActive() {
 
     var backgroundMenu = document.getElementsByClassName("background__sky")
     backgroundMenu [0].classList.toggle("background__sky--menu")
+}
+
+function changeReturnStepContent(currentPageNumber) {
+    var returnStepContent = document.getElementsByClassName("returnStepContent")
+    returnStepContent [currentPageNumber].classList.add("returnStepContent--inactive")
+    returnStepContent [currentPageNumber + 1].classList.remove("returnStepContent--inactive")
+}
+
+function returnInputExpand(currentField) {
+    var returnInputExpand = document.getElementsByClassName("return__input--expandable")
+    returnInputExpand[0].classList.toggle("return__input--active")
+
+    var savedAdress = document.getElementsByClassName("input__adress")
+    savedAdress[0].classList.remove("input__item--inactive")
+    savedAdress[1].classList.remove("input__item--inactive")
+    savedAdress[2].classList.remove("input__item--inactive")
+
+
+    returnInputExpand[1].classList.toggle("return__input--active")
+
+    var boxSize = document.getElementsByClassName("input__boxSize")
+    boxSize[0].classList.remove("input__item--inactive")
+    boxSize[1].classList.remove("input__item--inactive")
+    boxSize[2].classList.remove("input__item--inactive")
+}
+
+function changeCurrentAdress(number) {
+    var returnInputExpand = document.getElementsByClassName("return__input--expandable")
+    returnInputExpand[0].classList.toggle("return__input--active")
+    
+    var savedAdress = document.getElementsByClassName("input__adress")
+    
+    if (1 == number) {
+        savedAdress[1].classList.add("input__item--inactive")
+        savedAdress[2].classList.add("input__item--inactive")
+        savedAdress[3].classList.add("input__item--inactive")
+    }
+    if (2 == number) {
+        savedAdress[0].classList.add("input__item--inactive")
+        savedAdress[2].classList.add("input__item--inactive")
+        savedAdress[3].classList.add("input__item--inactive")
+    }
+    if (3 == number) {
+        savedAdress[0].classList.add("input__item--inactive")
+        savedAdress[1].classList.add("input__item--inactive")
+        savedAdress[3].classList.add("input__item--inactive")
+    }
+    if (4 == number) {
+        savedAdress[0].classList.add("input__item--inactive")
+        savedAdress[1].classList.add("input__item--inactive")
+        savedAdress[2].classList.add("input__item--inactive")
+    }
+}
+    
+function showMap() {
+    var map = document.getElementsByClassName("returnStep2__addAdress")
+    map[0].classList.toggle("returnStep2__addAdress--active")
+}
+
+function showMapAdress() {
+    var mapAdress = document.getElementsByClassName("returnStep2__mapAdress")
+    mapAdress[0].classList.add("returnStep2__mapAdress--active")
+
+   var map = document.getElementsByClassName("returnStep2__map")
+   map[0].classList.add("map--active")
+}
+
+function addAdress() {
+    var savedAdress = document.getElementsByClassName("input__adress")
+    savedAdress[3].classList.remove("input__item--inactive")
+    savedAdress[0].classList.add("input__item--inactive")
+    savedAdress[1].classList.add("input__item--inactive")
+    savedAdress[2].classList.add("input__item--inactive")
+
+    var input__line = document.getElementsByClassName("input__line")
+    input__line[2].classList.remove("input__line--inactive")
+
+    var map = document.getElementsByClassName("returnStep2__addAdress")
+    map[0].classList.remove("returnStep2__addAdress--active")
+}
+
+function boxSizeAvailability(boxSize) {
+    var availability = document.getElementsByClassName("boxSize__availability")
+    
+    var returnInputExpand = document.getElementsByClassName("return__input--expandable")
+    returnInputExpand[1].classList.toggle("return__input--active")
+
+    var changeBoxSize = document.getElementsByClassName("input__boxSize")
+    
+    if ("S" == boxSize) {
+        changeBoxSize[0].classList.remove("input__item--inactive")
+        changeBoxSize[1].classList.add("input__item--inactive")
+        changeBoxSize[2].classList.add("input__item--inactive")
+    }
+    if ("M" == boxSize) {
+        changeBoxSize[0].classList.add("input__item--inactive")
+        changeBoxSize[1].classList.remove("input__item--inactive")
+        changeBoxSize[2].classList.add("input__item--inactive")
+    }
+    if ("L" == boxSize) {
+        changeBoxSize[0].classList.add("input__item--inactive")
+        changeBoxSize[1].classList.add("input__item--inactive")
+        changeBoxSize[2].classList.remove("input__item--inactive")
+
+        availability[0].classList.add("boxSize__availability--inactive")
+        availability[1].classList.remove("boxSize__availability--inactive")
+    }
 }
