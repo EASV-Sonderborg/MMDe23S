781c69c7b3399252f7e62e7b99a6a80fd61ecca2
Sams-Design <148554049+Sams-Design@users.noreply.github.com>
2024-03-05 07:04:47 +0100

    js

---
+++ Samantha/javascript_test/js_first-steps/scriptArrays.js
@@ -0,0 +1,24 @@
+let notebook = [
+"Eier kaufen", 
+"Mehl kaufen", 
+"Putzen", 
+"Sport machen", 
+"Waesche waschen",];
+
+let newNote = "Mama anrufen";
+notebook.push(newNote);
+
+notebook.push("Milch kaufen"); //push weitere hinzufuegen
+
+
+
+console.log("Notiz 1 ist " + notebook[0]);
+console.log("Notiz 2 ist " + notebook[1]);
+
+console.log(notebook[0]);
+console.log(notebook[1]);
+console.log(notebook[2]);
+console.log(notebook[3]);
+console.log(notebook[4]);
+console.log(notebook[5]);
+console.log(notebook[6]);
