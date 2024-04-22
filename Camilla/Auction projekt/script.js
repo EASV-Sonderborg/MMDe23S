b836687d9b8f9de43332fa48129107b72f5529b7
Camilla <cammal01@easv365.dk>
2024-04-22 21:24:08 +0200

    Product page start

--- Camilla/Auction projekt/script.js
+++ Camilla/Auction projekt/script.js
@@ -1,22 +1,54 @@
-const URL = 
+const url = "https://web-auction-backend-production.up.railway.app/api/items";
 
-function getJSON(URL) {
-    // Make a GET request using the Fetch API
-    fetch(URL)
-      .then((response) => {
-        if (!response.ok) {
-          // If the request does not return 300 OK
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
-        // Pass the received data on to the function that formats it
-        console.log("Data received:", data);
-        formatData(data);
-      })
-      .catch((error) => {
-        // Handle errors
-        console.error("Error:", error);
-      });
-  }
+function getJSON(url) {
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP Error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("Data received:", data);
+      addData(data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+getJSON(url);
+
+const main = document.querySelector('.main')
+
+function addData(data){
+  console.log(data);
+data.forEach((item) =>{
+const productCard = document.createElement('section');
+productCard.classList.add('product');
+main.appendChild(productCard);
+  
+const productImg = document.createElement('img');
+productImg.src = item.imageSrc;
+productImg.classList.add('product__image');
+productCard.appendChild(productImg);
+
+
+
+const productTitle = document.createElement('h2');
+productTitle.textContent = item.title;
+productTitle.classList.add('product__title');
+productCard.appendChild(productTitle);
+
+const productPrice = document.createElement('p');
+productPrice.textContent=`Current Price: ${item.startPrice}`;
+productPrice.classList.add('product__price');
+productCard.appendChild(productPrice);
+
+const productDuration = document.createElement('p');
+productDuration.textContent=`Expires: ${item.expires}`;
+productDuration.classList.add('product__duration')
+productCard.appendChild(productDuration);
+})
+}
+
